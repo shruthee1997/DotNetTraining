a7b8bbf794978baf8a1a63d41ba975209063c3b3
shruthee1997 <60807136+shruthee1997@users.noreply.github.com>
2020-02-08 17:12:01 +0530

    Add files via upload

---
+++ Entity Framework/EF01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDBContext context = new ProductDBContext();
+            var Product = context.Products;
+
+            var product1 = Product.Add(new Product() { ProductID = 1, Name = "Surendar", Price = 99.99, Quantity = "500ml", CategoryID = 1 });
+
+            var product2 = Product.Add(new Product() { ProductID = 2, Name = "Suray", Price = 999.99, Quantity = "5000ml", CategoryID = 2 });
+
+            var product3 = Product.Add(new Product() { ProductID = 3, Name = "Pughal", Price = 1999.99, Quantity = "50000ml", CategoryID = 3 });
+
+            var Category = context.Categories;
+
+            var Category1 = Category.Add(new category() { CategoryID = 1, Name = "Lemon Juice"});
+
+            var Category2 = Category.Add(new category() { CategoryID = 2, Name = "fruit Juice" });
+        }
+    }
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext() : 
+            base("name=ProductDBContext")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<category> Categories { get; set; }
+    }
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Quantity { get; set; }
+        public  int CategoryID { get; set; }
+
+    }
+    public class category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Entity Framework/EF02.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstDemoProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDBContext context = new ProductDBContext();
+            var product = context.Products;
+            var product1 = product.Add(new Product()
+            {
+                ProductID = 1,
+                Name = "Badam",
+                Price = 99.90,
+                Quantity = "250ml",
+                CategoryID = 1
+            });
+            var product2 = product.Add(new Product()
+            {
+                ProductID = 2,
+                Name = "Coffee",
+                Price = 89.90,
+                Quantity = "250ml",
+                CategoryID = 2
+            });
+            var product3 = product.Add(new Product()
+            {
+                ProductID = 3,
+                Name = "Cold Coffee",
+                Price = 79.90,
+                Quantity = "550ml",
+                CategoryID = 3
+            });
+
+            var Category = context.Categories;
+
+            var Category1 = Category.Add(new category() { CategoryID = 3, Name = "Orange mint" });
+            var Category2 = Category.Add(new category() { CategoryID = 3, Name = "Lemon mint" });
+            context.SaveChanges();
+
+        }
+    }
+
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext() :
+            base("name=ProductDBContext")
+        { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<category> Categories { get; set; }
+    }
+
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Quantity { get; set; }
+        public int CategoryID { get; set; }
+
+    }
+    public class category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
