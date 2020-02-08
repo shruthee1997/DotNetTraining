1f915e0b820814969edcb311944557a4e24d64b6
shruthee1997 <60807136+shruthee1997@users.noreply.github.com>
2020-02-08 17:35:17 +0530

    Add files via upload

---
+++ Entity Framework/202002070459021_categoryAdded.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp16.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class categoryAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.categories",
+                c => new
+                    {
+                        CategoryID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Double(nullable: false),
+                        Quantity = c.String(),
+                        CategoryID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.categories");
+        }
+    }
+}
